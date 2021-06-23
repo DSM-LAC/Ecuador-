8d393d1386acf2cef9c212e6e46d3061feb9cb75
marioguevara <mguevara@udel.edu>
2021-06-23 10:30:02 -0400

    Update Taller_ECU_muestra.R

--- Taller_ECU_muestra.R
+++ Taller_ECU_muestra.R
@@ -1,37 +1,74 @@
+
 library(aqp)
+
 library(rgdal)
+
 library(GSIF)
+
 library(raster)
+
 library(elevatr)
 
+library(mapview)
 
 bd_taller_unida <- read.csv("Downloads/bd_taller_unida.csv")
 
-arcilla <- bd_taller_unida[c("ID_PER", "CORX.x", "CORY.x", "LIMSUP.x" , "LIMINF.x", "ARCILLA.x")]
+cos <- bd_taller_unida[c("ID_PER", "CORX.x", "CORY.x", "LIMSUP.x" , "LIMINF.x", "CO.x")]
 
-arcilla <- na.omit(arcilla)
+cos <- na.omit(cos)
 
-arcilla$IDPROF <- paste0("IDPROF_", arcilla$CORX.x, "_", arcilla$CORY.x)
+cos$IDPROF <- paste0("IDPROF_", cos$CORX.x, "_", cos$CORY.x)
 
-coordinates(arcilla) <- ~ CORX.x  +  CORY.x
+coordinates(cos) <- ~ CORX.x  +  CORY.x
 
-proj4string(arcilla) <-  CRS(" +init=epsg:32717")
+proj4string(cos) <-  CRS(" +init=epsg:32717")
 
-lim <- getData('GADM', country='ECU', level=2)
+lim <- raster::getData('GADM', country='ECU', level=1)
 
-lim <- spTransform(lim, CRS(projection(arcilla)))
+lim <- spTransform(lim, CRS(projection(cos)))
 
 ex <- extent(555809 , 579484.7 , 9848637, 9877033 )
 
 plot(ex)
 
-arcilla_crop <- crop(arcilla, ex)
+cos_crop <- crop(cos, ex)
 
-elev <- get_elev_raster(arcilla_crop, prj = projection(arcilla_crop), z = 10, clip = "bbox")
+mapview(cos_crop)
+
+sp4 <- as.data.frame(cos_crop)
+
+depths(sp4) <- ID_PER  ~ LIMSUP.x + LIMINF.x 
+
+plot(sp4[1:10], color='CO.x')
+
+site(sp4) <- ~ CORX.x + CORY.x
+
+coordinates(sp4) <- ~ CORX.x + CORY.x
+
+train <- data.frame()
+
+for (i in 1:length(sp4)){
+
+try(cos_sp <- mpspline(sp4[i], 'CO.x', d = t(c(0,30))))
+
+  cos_sp <- data.frame(
+  x = sp4[i]@sp@coords[,1],
+  y = sp4[i]@sp@coords[,2],
+  cossp_0100 = cos_sp$var.std[,1])
+
+  train <- rbind(train, cos_sp)
+
+  }
+
+coordinates(train) <- ~ x+y
+
+proj4string(train) <- CRS(projection(cos_crop))
+
+elev <- get_elev_raster(cos_crop, prj = projection(cos_crop), z = 10, clip = "bbox")
 
 plot(elev)
 
-plot(arcilla_crop, add=TRUE)
+plot(cos_crop, add=TRUE)
 
 x <- terrain(elev, opt=c('slope', 'aspect'), unit='degrees')
 
@@ -39,46 +76,36 @@ plot(x)
 
 
 # TPI for different neighborhood size:
-tpiw <- function(x, w=5) {
+tpiw <- function(x, w=15) {
   m <- matrix(1/(w^2-1), nc=w, nr=w)
   m[ceiling(0.5 * length(m))] <- 0
   f <- focal(x, m)
   x - f
 }
 
-tpi5 <- tpiw(elev, w=5)
+tpi5 <- tpiw(elev, w=15)
 
-covar <- stack(elev, tpi5, x)
+ndvi <- raster('Downloads/ndvi.tif')
 
-sp4 <- as.data.frame(arcilla_crop)
+ndvi <- projectRaster(ndvi, elev)
 
-depths(sp4) <- ID_PER  ~ LIMSUP.x + LIMINF.x 
+covar <- as(stack(elev, tpi5, x, ndvi), 'SpatialPixelsDataFrame')
 
-#define the coordinates for sampling sites
-site(sp4) <- ~ CORX.x + CORY.x
-#define the same coordinates
-coordinates(sp4) <- ~ CORX.x + CORY.x
+names(covar) <- c('elev', 'tpi', 'slope', 'aspect')
 
-train <- data.frame()
+plot(elev)
 
-for (i in 50:length(sp4)){#2 #49 #problemas en
-#performa a mass preservative spline to estimate ECe at 0-100 cm
-try(arcilla_sp <- mpspline(sp4[i], 'ARCILLA.x', d = t(c(0,5))))
+plot(train, add=TRUE)
 
-arcilla_sp <- data.frame(
-  #we include coordinates
-  x = sp4@sp@coords[,1],
-  y = sp4@sp@coords[,2],
-  #the ID for the site
-  site=sp4@site,
-  # and the values of ECe
-  ARCILLAsp_0100 = arcilla_sp$var.std[,1])
-   
-train <- rbind(train, arcilla_sp)
+mapview(train['cossp_0100'])
 
-#rm(arcilla_sp)
+train@data <- cbind(train@data, over(train, covar))
 
-}
+#train$cossp_0100 <- log1p(train$cossp_0100)
 
+m <- train.spLearner(train["cossp_0100"], covariates=covar, lambda = 1)
 
-coordinates(arcilla) <- ~ CORX.x + CORY.x
+summary(m@spModel$learner.model$super.model$learner.model)
+
+mp <- predict(m)
+
